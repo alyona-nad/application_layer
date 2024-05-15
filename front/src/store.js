fd94700dee227daa96547e0961774dcddd2beb8e
alyona-nad <116522266+alyona-nad@users.noreply.github.com>
2024-05-15 22:38:02 +0300

    Delete front directory

--- front/src/store.js
+++
@@ -1,7 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit'
-import messageReducer from "./features/messages/messageSlice";
-export const store = configureStore({
-    reducer: {
-        message: messageReducer
-    },
-})
